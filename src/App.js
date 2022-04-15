1f99f087cb3e39af9f60279fe0edb065d9d8616c
Eddy <eduard.kosian@itsolutions.ee>
2022-04-15 13:18:14 +0400

    Add News, Music, Settings pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import MessagesContainer from "./components/Messages/MessagesContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import Music from "./components/Music/Music";
+
 
 function App() {
   return (
@@ -17,6 +21,9 @@ function App() {
           <Route path="/profile/*" element={<ProfileContainer />} />
           <Route path="/messages/*" element={<MessagesContainer />} />
           <Route path="/users/*" element={<UsersContainer />} />
+          <Route path="/news/*" element={<News />} />
+          <Route path="/music/*" element={<Music />} />
+          <Route path="/settings/*" element={<Settings />} />
         </Routes>
       </main>
     </div>
