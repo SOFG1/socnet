f5ba218b902f44cd7022b25cb4bbd93bbb78a01c
Eddy <eduard.kosian@itsolutions.ee>
2022-04-21 14:49:34 +0400

    Implement React.memo on Posts component

--- src/components/Profile/Posts.jsx
+++ src/components/Profile/Posts.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 import s from "./Profile.module.scss";
 import PostsForm from "./PostsForm";
-import Post from './Post'
+import Post from "./Post";
 
-class Posts extends React.Component {
-
-  componentDidUpdate() {
-    console.log('updated')
-  }
-  
-  render() {
-    return (
-      <div className={s.Posts}>
-        <h2 className={s.title}>MyPosts</h2>
-        <PostsForm onSubmit={this.props.addPost} />
-        {this.props.posts
-          .map((post) => {
-            return (
-              <Post key={post.id} post={post} likePost={this.props.likePost} />
-            );
-          })
-          .reverse()}
-      </div>
-    );
-  }
-};
+const Posts = React.memo((props)=> {
+  return (
+    <div className={s.Posts}>
+      <h2 className={s.title}>MyPosts</h2>
+      <PostsForm onSubmit={props.addPost} />
+      {props.posts
+        .map((post) => {
+          return (
+            <Post key={post.id} post={post} likePost={props.likePost} />
+          );
+        })
+        .reverse()}
+    </div>
+  );
+})
 
 export default Posts;
