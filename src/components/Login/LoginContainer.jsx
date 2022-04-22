1d0c211b48a2e4b0646f3d7a5b6eb356af768fc6
Eddy <eduard.kosian@itsolutions.ee>
2022-04-22 23:52:39 +0400

    Convert Status to a functional component

--- src/components/Login/LoginContainer.jsx
+++ src/components/Login/LoginContainer.jsx
@@ -2,21 +2,16 @@ import React from "react";
 import Login from "./Login";
 import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
-import {loginThunk as login} from '../../redux/authReducer'
+import { loginThunk as login } from "../../redux/authReducer";
 
+const LoginContainer = (props) => {
+  return props.auth.isAuth ? <Navigate to="/" /> : <Login {...props} />;
+};
 
-class LoginContainer extends React.Component {
-    render() {
-        return this.props.auth.isAuth ? <Navigate to="/" /> :<Login {...this.props} />
-    }
-}
+let mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
 
-let mapStateToProps = (state)=> {
-    return {
-        auth: state.auth,
-    }
-}
-
-
-
-export default connect(mapStateToProps, {login})(LoginContainer);
+export default connect(mapStateToProps, { login })(LoginContainer);
--- src/components/common/Status/Status.jsx
+++ src/components/common/Status/Status.jsx
@@ -1,80 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { statusValidator } from "../../../utilites/validators";
 import s from "./Status.module.scss";
 
-class Status extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: props.status,
-      editMode: false,
-      error: false,
-    };
-    this.onChange = this.onChange.bind(this);
-    this.toggleEdit = this.toggleEdit.bind(this);
-    this.onBlur = this.onBlur.bind(this);
+const Status = (props)=> {
+  const [status, setStatus] = useState(null);
+  const {editMode, setEditMode} = useState(false);
+  const {error, setError} = useState(false);
+  useEffect(()=> {
+    setStatus(props.status)
+  }, [props])
+  let onChange= (e)=> {
+    setStatus(e.target.value);
+    setError(!statusValidator(e.target.value));
   }
-  componentDidUpdate(oldProps, oldState) {
-    if (oldProps.status !== this.props.status)
-      this.setState({ status: this.props.status });
+  let toggleEdit = ()=> {
+    if (!props.readOnly) setEditMode(mode => !mode);
   }
-  onChange(e) {
-    this.setState({
-      status: e.target.value,
-      error: !statusValidator(e.target.value),
-    });
-  }
-  toggleEdit() {
-    if (!this.props.readOnly) this.setState({ editMode: !this.state.editMode });
-  }
-  onBlur(e) {
+  let onBlur = (e)=> {
     const valid = statusValidator(e.target.value);
     if (valid) {
-      this.setState({ status: this.props.status });
-      this.toggleEdit();
+      setStatus(props.status);
+      setEditMode(mode => !mode);
     }
-    if (this.props.status !== this.state.status && valid) {
-      this.props.changeStatus(e.target.value);
+    if (props.status !== status && valid) {
+      props.changeStatus(e.target.value);
     }
     if (!statusValidator(e.target.value)) {
-      this.setState({ error: true });
+      setError(true)
     }
   }
-  render() {
-    return (
-      (this.props.status || !this.props.readOnly) &&
-      
-      <div
-        className={
-          this.props.readOnly ? s.statusBox : `${s.statusBox} ${s.editable}`
-        }
-        onDoubleClick={this.toggleEdit}
-      >
-        {this.state.editMode ? (
-          <input
-            type="text"
-            value={this.state.status}
-            onChange={this.onChange}
-            onBlur={this.onBlur}
-            autoFocus
-          />
-        ) : (
-          <p className={s.status}>
-            {this.state.status}
-            {this.state.status.length === 0 && !this.props.readOnly && (
-              <span className={s.empty}>Click to set the status...</span>
-            )}
-          </p>
-        )}
-        {this.state.error && (
-          <p className={s.error}>
-            Status must container letters or numbers and not exceed 300
-            characters
-          </p>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    (props.status || !props.readOnly) &&
+    <div
+      className={
+        props.readOnly ? s.statusBox : `${s.statusBox} ${s.editable}`
+      }
+      onDoubleClick={toggleEdit}
+    >
+      {editMode ? (
+        <input
+          type="text"
+          value={status}
+          onChange={onChange}
+          onBlur={onBlur}
+          autoFocus
+        />
+      ) : (
+        <p className={s.status}>
+          {status}
+          {status.length === 0 && !props.readOnly && (
+            <span className={s.empty}>Click to set the status...</span>
+          )}
+        </p>
+      )}
+      {error && (
+        <p className={s.error}>
+          Status must container letters or numbers and not exceed 300
+          characters
+        </p>
+      )}
+    </div>
+  )
+} 
+
 
 export default Status;
