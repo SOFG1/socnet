bd15395da765687aa58e6ce28207522d94a5e696
Eddy <edkosyan2000@gmail.com>
2022-11-02 13:23:16 +0400

    add follow user thunk test

--- src/redux/follow-thunk.test.ts
+++ src/redux/follow-thunk.test.ts
@@ -1,4 +1,17 @@
 import { followThunk } from "./usersReducer"
+import { followApi } from "../api/api.ts";
+jest.mock('../api/api.ts')
+
+const followApiMock = followApi
+
+
+followApiMock.followUser = () => {
+    return new Promise((res) => {
+        setTimeout(() => {
+            res(0)
+        }, 2000)
+    })
+}
 
 
 test("follow thunk test", async  () => {
